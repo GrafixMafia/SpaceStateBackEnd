06d2890d504b3fb74d19df75d6b38b64df887d87
Markus Müller <reg@grafixmafia.net>
2015-05-03 13:23:16 +0200

    deconstruct json space feedback

--- src/mainFrame_SpaceStateUtils.erl
+++ src/mainFrame_SpaceStateUtils.erl
@@ -4,25 +4,26 @@
 
 -export ([convertSpaceState/1]).
 
-
 convertSpaceState(SpaceStateIn) ->
     {SpaceState} = SpaceStateIn,
-    iterateUntil(SpaceState, <<"name">>),
+    iterateUntil(SpaceState, <<"space">>),
     {ok, ok}.
 
+iterateUntil([], Item) ->
+    {ok, nothing};
 iterateUntil([H|T],Item) ->
     case is_list(H) of 
         true -> io:format("No list no tuple ~n");
-        false -> selectFromTuple(H, Item)
+        false -> selectFromTuple(H,T , Item)
     end.
 
-selectFromTuple(Pair, Item) -> 
+selectFromTuple(Pair, T , Item) -> 
     case is_tuple(Pair) of
-        true -> decompose(tuple_to_list(Pair), Item);
-        false -> io:format("No tuple no List ~n")
+        true -> decompose(tuple_to_list(Pair), T ,Item);
+        false -> iterateUntil(T, Item)
     end.
-decompose([H|T], Item) -> 
+decompose([H|T], L, Item) -> 
     case H == Item of
         true -> io:format("Bin kene Liste ~p ~n", [T]);
-        false -> io:format("Nothing ~n")
+        false -> iterateUntil(L, Item)
     end.
